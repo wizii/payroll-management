f4f5cd2b916f283a2dc31deacfded88197c49ca6
wizii <wizim393@gmail.com>
2024-07-05 00:44:49 +0400

    Remove comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,14 @@ import type { NextRequest } from 'next/server'
 import { withMiddlewareAuthRequired, getSession } from '@auth0/nextjs-auth0/edge';
  
 export default withMiddlewareAuthRequired(async function middleware(request: NextRequest) {
-// const { user } = await getSession();
-// console.log(user)
+  const appSession = request.cookies.get('appSession')?.value;
 
-  const appSession = request.cookies.get('appSession')?.value
-
-  console.log(appSession)
- 
   if (appSession && !request.nextUrl.pathname.startsWith('/')) {
-    return Response.redirect(new URL('/', request.url))
+    return Response.redirect(new URL('/', request.url));
   }
  
   if (!appSession && !request.nextUrl.pathname.startsWith('/')) {
-    return Response.redirect(new URL('/login', request.url))
+    return Response.redirect(new URL('/login', request.url));
   }
 });
  
