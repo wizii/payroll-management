d24a06ef80ef1c3ac78e3104f0f74ede1c4183fc
wizii <wizim393@gmail.com>
2024-07-11 23:01:30 +0400

    Icons

--- src/app/components/side-menu-item.tsx
+++ src/app/components/side-menu-item.tsx
@@ -2,15 +2,17 @@
 import Link from 'next/link';
 import { useGlobal } from "../context/globalContext";
 import { useEffect, useState } from 'react';
+import { dividerClasses } from '@mui/material';
 
 type SideMenuItemProps = {
     label: string;
     route: string;
+    iconPath?: string;
 }
 
 // TODO: icons
 export default function SideMenuItem(props: SideMenuItemProps) {
-    const {route, label} = props;
+    const {route, label, iconPath} = props;
     const { pageTitle } = useGlobal();
     const [isActive, setIsActive] = useState(pageTitle === label);
 
@@ -19,7 +21,10 @@ export default function SideMenuItem(props: SideMenuItemProps) {
     }, [pageTitle, label]);
 
     return (
-        <div className="p-2 text-center">
+        <div className="p-2 text-center flex items-center">
+            {iconPath &&
+                <div className={`bg-url[${iconPath}`}></div>
+            }
             <Link className={`${isActive ? 'text-[#ff220f]' : ''}`} href={route}>{label}</Link>
         </div>
     );
